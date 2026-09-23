65c4ca777ad1a3370b48f3a7294d5ab4a622dfe9
unknown <irtacapkova@gmail.com>
2026-09-23 17:19:42 +0300

    aaaaaaaa

--- src/lab2/tuples.py
+++ src/lab2/tuples.py
@@ -2,6 +2,8 @@ def format_record(rec: tuple):
     rec_list = list(rec)
     el1 = rec_list[0].split()
     res = el1[0].capitalize() + ' '
+    if rec_list[2] > 5 or rec_list[2]<0:
+        return ValueError
     for i in range(1,len(el1)):
         res += (el1[i][0]).upper() + '.'
     res = f'{res}, гр. {rec_list[1]}, GPA {rec_list[2]:.2f}'
